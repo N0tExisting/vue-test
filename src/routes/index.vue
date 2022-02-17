f21954096a0bc1a0ead825e1f954d67eb550e760
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2022-02-17 19:39:24 +0100

    feat: add index page

--- src/routes/index.vue
+++ src/routes/index.vue
@@ -1,12 +1,51 @@
 <script setup lang="ts">
-	import logo from '$asset/logo.svg?url';
+	import { onMounted, onBeforeUnmount, ref } from 'vue';
+
+	const time = ref(0);
+
+	onMounted(() => {
+		const interval = setInterval(() => {
+			time.value++;
+		}, 1000);
+
+		onBeforeUnmount(() => {
+			clearInterval(interval);
+		});
+	});
 </script>
 
 <template>
-	<main>
+	<main class="h-full flex flex-col place-content-center place-items-center text-lg">
 		<!-- TODO: Animate Logo
 			(Up & down while squishing)?
 		-->
-		<img alt="Vue logo" class="logo" :src="logo" width="125" height="125" />
+		<img
+			alt="Vue logo"
+			class="pointer-events-none h-40 m-4 select-none"
+			src="../assets/logo.svg?url" />
+
+		<p>Edit <code>routes/index.vue</code> and save to test HMR updates.</p>
+
+		<p class="pt-3">
+			<a
+				class="text-cyan-600 dark:text-cyan-400"
+				href="https://vuejs.org"
+				target="_blank"
+				rel="noopener noreferrer">
+				Learn Vue.js
+			</a>
+			|
+			<a
+				class="text-cyan-600 dark:text-cyan-400"
+				href="https://vitejs.dev/"
+				target="_blank"
+				rel="noopener noreferrer">
+				Vite Docs
+			</a>
+		</p>
+
+		<p class="pt-3">
+			This Page Has been open for <code>{{ time }}</code> seconds.
+		</p>
 	</main>
 </template>
