c1e8f96e61c1e5349d2137867ce2835474728475
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2022-02-23 18:37:37 +0100

    chore: move favicon to assets

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 	import { RouterView } from 'vue-router';
 	import { Head } from '@vueuse/head';
 	import NavBar from '$layout/NavBar.vue';
+	import favicon from '$assets/favicon.ico?url';
 </script>
 
 <template>
 	<Head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Vite App</title>
-		<link rel="icon" href="/favicon.ico" />
+		<link rel="icon" :href="favicon" />
 	</Head>
 
 	<NavBar />
