3ff02aa94a2112e58f7de070a0d6cb2e0c077d20
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2022-03-10 17:01:04 +0100

    feat: Update About Page

--- cypress/integration/about.spec.ts
+++ cypress/integration/about.spec.ts
@@ -1,7 +1,10 @@
 describe('About Page', () => {
 	it('renders', () => {
 		cy.visit('/about');
+
 		cy.contains('h1', 'About this Website');
+		cy.contains('p', 'This is Website was made with Vite and Vue.');
+
 		cy.screenshot();
 	});
 });
--- src/routes/about.vue
+++ src/routes/about.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-	import { useHead } from '@vueuse/head';
-	useHead({
-		title: 'About',
-		meta: [
-			{
-				name: 'description',
-				content: 'This is the about page',
-			},
-		],
-	});
+	import { Head } from '@vueuse/head';
 </script>
 
 <template>
-	<div class="flex justify-center text-center">
-		<h1 class="text-6xl">About this Website</h1>
+	<div class="h-full flex flex-col justify-center text-center">
+		<Head>
+			<title>About</title>
+			<meta name="description" content="About this Website" />
+		</Head>
+		<h1 class="text-6xl mb-2">About this Website</h1>
+
+		<div
+			class="h-full flex flex-col justify-center content-center text-center text-3xl">
+			<p>
+				This is Website was made with
+				<a
+					class="text-cyan-600 dark:text-cyan-400"
+					href="https://vitejs.dev/"
+					target="_blank"
+					rel="noopener noreferrer">
+					Vite</a
+				>
+				and
+				<a
+					class="text-cyan-600 dark:text-cyan-400"
+					href="https://vuejs.org"
+					target="_blank"
+					rel="noopener noreferrer">
+					Vue </a
+				>.
+			</p>
+		</div>
 	</div>
 </template>
